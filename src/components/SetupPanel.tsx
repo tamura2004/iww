28f3a93eccceac5b7f4161e60a89371f28c6dab2
tamura2004 <tamura2004@gmail.com>
2025-03-19 10:56:46 +0000

    Add setup panel

--- src/components/SetupPanel.tsx
+++ src/components/SetupPanel.tsx
@@ -1,17 +1,37 @@
 import TabPanel from "@mui/lab/TabPanel";
-import {Box, InputLabel, MenuItem, Select, TextField} from "@mui/material";
+import {
+  Autocomplete,
+  Box,
+  Button,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useState } from "react";
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 
 export const SetupPanel = () => {
   const [numberOfPlayers, setNumberOfPlayers] = useState(3);
   return (
     <TabPanel value="設定" sx={{ padding: 0 }}>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 4, margin: 4 }}>
-        <Box sx={{ display: "flex", gap: 4 }}>
-          <InputLabel sx={{ fontSize: 48 }} id="number-of-players">
+        <Button
+          variant="contained"
+          sx={{
+            fontSize: { sm: "3vh", xs: "2.5vh" },
+          }}
+          disabled={false}
+        >
+          新しいゲームを開始する
+        </Button>
+        <Box sx={{ display: "flex", gap: 4, alignItems: "center" }}>
+          <Typography
+            sx={{ fontSize: { sm: "3vh", xs: "2.5vh" } }}
+            id="number-of-players"
+          >
             プレイヤー数
-          </InputLabel>
+          </Typography>
           <Select
             variant={"standard"}
             labelId="number-of-players"
@@ -21,26 +41,41 @@ export const SetupPanel = () => {
             onChange={(e) => {
               setNumberOfPlayers(e.target.value as number);
             }}
-            sx={{ fontSize: 48 }}
+            sx={{ fontSize: { sm: "3vh", xs: "2.5vh" } }}
           >
-            {[1, 2, 3, 4, 5, 6, 7].map((i) => (
-              <MenuItem sx={{ fontSize: 48 }} value={i}>
-                {i}人
+            {[...Array(7)].map((_, i) => (
+              <MenuItem
+                sx={{ fontSize: { sm: "3vh", xs: "2.5vh" } }}
+                value={i + 1}
+              >
+                {i + 1}人
               </MenuItem>
             ))}
           </Select>
-          <TextField
-            variant="standard"
-            value={numberOfPlayers}
-            onChange={(e) => setNumberOfPlayers(Number(e.target.value))}
-            placeholder="カスタム"
-            sx={{ fontSize: 48 }}
-          />
         </Box>
         {[...Array(numberOfPlayers)].map((_, i) => (
-          <Box key={i} sx={{ fontSize: 48 }}>
-            <AccountCircleIcon sx={{ fontSize: 48 }} />
-            プレイヤー{i + 1}
+          <Box sx={{ display: "flex", flexDirection: "row", gap: 2 }}>
+            <Box
+              key={i}
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <AccountCircleIcon sx={{ fontSize: 48 }} />
+              プレイヤー{i + 1}
+            </Box>
+            <Autocomplete
+              options={["alice", "bob"]}
+              sx={{ width: "30%" }}
+              renderInput={(params) => <TextField {...params} label="名前" />}
+            />
+            <Autocomplete
+              options={["north america", "aztec", "asia"]}
+              sx={{ width: "50%" }}
+              renderInput={(params) => <TextField {...params} label="帝国" />}
+            />
           </Box>
         ))}
       </Box>
